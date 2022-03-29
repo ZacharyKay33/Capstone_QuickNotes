c1a7409f087c08ffb3e2b0408f53ea6c8b62ec0b
Guysnacho <guysnacho2@gmail.com>
2022-03-29 12:35:23 -0500

    Add icons and finish laying out login page
    Used an icon for the musical note but moving forward I'm just gonna add
    pictures to the assets folder. Didn't at first because that increases
    bundle size but we don't care atm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,30 @@
 import { Grid, Typography } from "@mui/material";
+import Login from "../components/Login";
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+/* const LastFM = require("last-fm");
+const lastfm = new LastFM(process.env.LASTFM_KEY, {
+  userAgent: "QuarterNotes/0.0.1 (http://quarternotes.com)",
+}); */
 
 const Home: NextPage = () => {
+  //Page's state (data)
   const [isAuthed, setIsAuthed] = useState(false);
+
+  //Side effect that fires whenever isAuthed is updated
+  useEffect(() => {
+    console.log("isAuthed is updated to - " + isAuthed);
+  }, [isAuthed]);
 
   return (
     <>
-      {isAuthed ? (
+      {isAuthed ? ( //Check if we're authenticated
         <Grid container justifyContent="center">
           <Typography variant="h4">Home</Typography>
         </Grid>
       ) : (
-        <Grid container justifyContent="center">
-          <Typography variant="h4">Login</Typography>
-        </Grid>
+        <Login />
       )}
     </>
   );
