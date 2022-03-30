00fabedf677e79acd101fde2f5e486a93d77af8d
Guysnacho <guysnacho2@gmail.com>
2022-03-30 12:54:39 -0500

    fix selector

--- redux/userSlice.ts
+++ redux/userSlice.ts
@@ -43,6 +43,6 @@ const userSlice = createSlice({
 export const { login, logout } = userSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectUser = (state: RootState) => state.user.user;
+export const selectUser = (state: RootState) => state.user;
 
 export default userSlice.reducer;
