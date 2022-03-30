f28a196615dce51939b5e3d192ec772002cd4846
Guysnacho <guysnacho2@gmail.com>
2022-03-30 13:47:42 -0500

    push to home if authed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,22 @@
 import Enter from "../components/Enter";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
+import { useAppSelector } from "../redux/hooks";
 
 const Splash: NextPage = () => {
   const router = useRouter();
+
   //Page's state (data)
-  const [isAuthed, setIsAuthed] = useState(false);
+  const uid = useAppSelector(state => state.user.uid);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  //Side effect that fires whenever isAuthed is updated
-  useEffect(() => {
-    console.log("isAuthed is updated to - " + isAuthed);
-  }, [isAuthed]);
-
   return (
     <>
-      {isAuthed ? ( //Check if we're authenticated
-        router.replace("/Home")
+      {uid !== "" && uid !== null && uid !== undefined ? ( //Check if we're authenticated
+        router.push("/Home")
       ) : (
         <Enter />
       )}
