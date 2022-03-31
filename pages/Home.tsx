877cb676118b8b3c9c2b0f55419012dcbe9f730e
Guysnacho <guysnacho2@gmail.com>
2022-03-31 12:15:15 -0500

    Adding api route

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,14 @@
 import { Grid, Typography } from "@mui/material";
 import { NextPage } from "next";
 
+/* export async function getServerSideProps(context: NextPageContext) {
+  return {
+    props: {
+      session: await getSession()
+    }, // Will be passed to the page component as props
+  };
+} */
+
 const Home: NextPage = () => {
   return (
     <Grid container justifyContent="center">
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -3,6 +3,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
   Auth,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -17,22 +18,18 @@ type Data = {
 
 type Response = { user: {}; message: string; code: number };
 
-const createUser = (payload: Data): Response => {
+/*API Methond
+Name - authUser
+Input - Payload in the above Data format
+Output - a Response object
+Purpose - Used when logging into system
+*/
+const authUser = (payload: Data): Response => {
   let response = { user: {}, message: "", code: 200 };
   const auth = payload.auth;
-  createUserWithEmailAndPassword(
-    auth,
-    payload.user.email,
-    payload.user.password
-  )
+  signInWithEmailAndPassword(auth, payload.user.email, payload.user.password)
     .then((userCredential) => {
       //Signed In
-      if (auth.currentUser !== null) {
-        updateProfile(auth.currentUser, {
-          // Updating the firebase username
-          displayName: payload.user.username,
-        });
-      }
       if (userCredential.user !== null) {
         //Sending the user back as a response
         response.user = userCredential.user;
@@ -53,9 +50,53 @@ const createUser = (payload: Data): Response => {
   });
 };
 
+/*API Methond
+Name - createUser
+Input - Payload in the above Data format
+Output - a Response object
+Purpose - Used when creating a user
+*/
+const createUser = (payload: Data): Response => {
+  let response = { user: {}, message: "", code: 200 };
+  const auth = payload.auth;
+  createUserWithEmailAndPassword(
+    auth,
+    payload.user.email,
+    payload.user.password
+  )
+    .then((userCredential) => {
+      //Signed In
+      if (userCredential.user !== null) {
+        updateProfile(userCredential.user, {
+          // Updating the firebase username
+          displayName: payload.user.username,
+        });
+        //Sending the user back as a response
+        response.user = userCredential.user;
+        response.code = 200;
+      }
+      return response;
+    })
+    .catch((error) => {
+      // Catching errors if there are any
+      response.message = error.message;
+      response.code = error.code;
+      return response;
+    });
+  return (response = {
+    user: {},
+    code: 400,
+    message: "Bad request, atrocious even.",
+  });
+};
+
+//Recieves all requests, routes them to the correct method, returns a response
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     let response = createUser(req.body);
+    res.status(response.code).json(response.user);
+  } else if (req.method === "GET") {
+    let response = authUser(req.body);
     res.status(response.code).json(response.user);
   }
   res.status(400).json({ user: {} });
