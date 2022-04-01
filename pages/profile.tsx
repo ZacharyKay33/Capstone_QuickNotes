a2254bd8c5b05484229fb11742c3974e90815006
Guysnacho <guysnacho2@gmail.com>
2022-03-31 22:17:21 -0500

    Figured out image button

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,81 +1,84 @@
 import {
   Grid,
   Paper,
-  List,
   Avatar,
   Typography,
-  TextField,
   Button,
-  FormControl,
-  ListItemAvatar,
-  ListItemText,
   Stack,
+  Box,
+  Input,
+  TextField,
 } from "@mui/material";
-import "./api/Firebase";
-
-/* class profile extends React.Component{
-    state = {
-        user: null
-    };
-
-    componentDidMount() {
-        const {uid} = this.props
-
-        const isauth = auth.user.id == uid;
-        getUser(uid).then(user =>{
-            this.setState({
-                user,
-                isauth
-            });
-        });
-    }
-
-    render(){
-
-        const{classes} = this.props;
-        const{user, isauth} = this.state;
-        return(
-            <Paper className = {classes.root} elevation = {4}>
-                <Typography
-                    variant = "h4"
-                    component "h1"
-                    align = "center"
-                    className = {classes.title}
-                    gutterBottom
-                    >
-                    
-                    profile
-                    </Typography>
-                (
-                <List dense>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar src = {user.avatar} className = {classes.bigAvatar} /> 
-                        </ListItemAvatar>
-                        <ListItemText primary = {user.name} secondary = {user.bio} />
-                    </ListItem>
-                </List>
-                )
-            </Paper>
-        ) 
-        
-    }
-} */
-
-//const profile = () => {
-
-//}
+import { SetStateAction, useEffect, useState } from "react";
+import "./api/Firebase"; //W peace and love
 
 const Profile = () => {
+  //State definitions
+  const [editing, setEditing] = useState(false);
+  const [profilePic, setProfilePic] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [username, setUsername] = useState("Jared");
+  const [bio, setBio] = useState("Jared's Biography");
+
+  //Functions
+  const updUsername = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setUsername(event.target.value);
+  };
+
+  useEffect(() => {
+    if (profilePic) {
+      setImageUrl(URL.createObjectURL(profilePic));
+    }
+  }, [profilePic]);
+
   return (
     <Paper elevation={4}>
       <Stack spacing={3}>
         <Typography variant="h4" align="center" gutterBottom>
           Profile
         </Typography>
-        <Avatar sx={{ width: 200, height: 200 }}>T</Avatar>
-        <Typography>User.name</Typography>
-        <Typography>User.bio</Typography>
+        <Box>
+          {imageUrl ? (
+            <Avatar
+              sx={{ width: 200, height: 200, m: "auto" }}
+              src={imageUrl}
+            />
+          ) : (
+            <Avatar sx={{ width: 200, height: 200, m: "auto" }} src="">
+              T
+            </Avatar>
+          )}
+        </Box>
+        <Typography align="center">{username}</Typography>
+        <Typography align="center">{bio}</Typography>
+        <Input
+          accept="image/*"
+          id="contained-button-file"
+          multiple
+          type="file"
+          onChange={(e) => setProfilePic(e.target.files[0])}
+        />
+        <Button
+          onClick={() => {
+            setEditing(!editing);
+          }}
+        >
+          {editing ? "Done Editing" : "Edit Profile"}
+        </Button>
+        {editing ? (
+          <>
+            <Grid container>
+              <TextField label="username" onChange={updUsername} />
+            </Grid>
+            <Grid container>
+              <TextField label="bio" fullWidth />
+            </Grid>
+          </>
+        ) : (
+          <></>
+        )}
       </Stack>
     </Paper>
   );
