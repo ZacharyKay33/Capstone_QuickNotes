e8be78bdd8956eaeda4bbc19e8ab89efa5c1489e
Guysnacho <guysnacho2@gmail.com>
2022-03-31 23:46:55 -0500

    Add input

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,5 +1,6 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Stack, TextField, Typography } from "@mui/material";
 import { NextPage } from "next";
+import { SetStateAction, useState } from "react";
 
 /* export async function getServerSideProps(context: NextPageContext) {
   return {
@@ -10,9 +11,53 @@ import { NextPage } from "next";
 } */
 
 const Home: NextPage = () => {
+  const [song, setSong] = useState("");
+  const [title, setTitle] = useState("");
+  const [review, setReview] = useState("");
+
+  const updSong = (event: { target: { value: SetStateAction<string> } }) => {
+    setSong(event.target.value);
+  };
+  const updTitle = (event: { target: { value: SetStateAction<string> } }) => {
+    setTitle(event.target.value);
+  };
+  const updReview = (event: { target: { value: SetStateAction<string> } }) => {
+    setReview(event.target.value);
+  };
   return (
     <Grid container justifyContent="center">
-      <Typography variant="h4">Home</Typography>
+      <Stack direction="column" spacing={3}>
+        <Typography variant="h4" align="center">
+          Home
+        </Typography>
+        <TextField
+          id="song-input"
+          label="Song Title"
+          placeholder="Super Bounce"
+          value={song}
+          onChange={updSong}
+          aria-label="song-input"
+          fullWidth
+        />
+        <TextField
+          id="title-input"
+          label="Review Title"
+          placeholder="I think it's pretty sick"
+          value={title}
+          onChange={updTitle}
+          aria-label="title-input"
+          fullWidth
+        />
+        <TextField
+          id="review-input"
+          label="Review"
+          placeholder="Mellon Head"
+          value={review}
+          onChange={updReview}
+          aria-label="review-input"
+          fullWidth
+        />
+      </Stack>
     </Grid>
   );
 };
