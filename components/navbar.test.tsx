f8ac8eae4243fac0d0bddf53c1a0804319065523
babybbuster <74384431+babybbuster@users.noreply.github.com>
2022-04-30 20:25:55 -0500

    Update navbar.test.tsx

--- components/navbar.test.tsx
+++ components/navbar.test.tsx
@@ -1,7 +1,19 @@
 import React from 'react';  
 import { render, screen } from '@testing-library/react'; 
 import Navbar from './Navbar'; // component to test
-test('render profile link', () => {
-  render(<Navbar />);
-  expect(screen.getByText(/profile/)).toBeInTheDocument();
-})
+const links = [
+  { text: 'Logout', location: "/" },
+  { text: 'Home', location: "/home" },
+  { text: 'Profile', location: "/profile" },
+  { text: 'Search', location: "/search" },
+  
+  //test every link
+  test.each(links)(
+  "Click the links to check if Nav Bar has %s link.",
+  (link) => {
+    render(<NavBar />); 
+    expect(links).toHaveAttribute("href", link.location);
+  }
+test('Check if have link to page', () => {
+    render(<NavBar />);
+    expect(links).toHaveAttribute("href", "/"); 
